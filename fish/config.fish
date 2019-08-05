5973bda453497a461c65171efed6054bf40c0523
shino <sc@shinomin.me>
2019-08-05 21:17:27 +0700

    cleanup

--- fish/config.fish
+++ fish/config.fish
@@ -12,16 +12,7 @@ set HISTFILESIZE  10000
 # Homebrew
 set -x PATH $PATH /usr/local/sbin
 
-# Go
-set -x GOPATH $HOME/go
-
-# Rust
-set -x PATH $PATH $HOME/.cargo/bin
-
 
 # Aliases
 alias lt "ls -lrt"
-
-alias gcc "gcc-9"
-alias g++ "g++-9"
 
--- fish/functions/cheat.fish
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env fish
-
-function cheat --description "cheat <command>"
-    curl cht.sh/"$argv[1]"
-end
-
