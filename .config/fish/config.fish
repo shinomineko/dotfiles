b81a4e45e6f2ef64870a7be42f90a611174f856f
shinomineko <sc@shinomin.me>
2024-10-30 22:15:30 +0700

    source .extra.fish if exists

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     source ~/.config/fish/aliases.fish
     source ~/.config/fish/env.fish
+
+    if test -f ~/.extra.fish
+        source ~/.extra.fish
+    end
 end
