3b57d20d090a440ce3ddc222cee5ec41780fe5b9
shino <sc@shinomin.me>
2024-12-02 17:36:00 +0700

    update EDITOR

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -1,12 +1,14 @@
-set -Ux EDITOR vim
-set -Ux VISUAL vim
+# set -Ux EDITOR vim
+# set -Ux VISUAL vim
+set -Ux EDITOR hx
+set -Ux VISUAL hx
 set -gx LC_ALL en_US.UTF-8
 set -gx LANG en_US.UTF-8
 
 fish_add_path "$HOME/.local/bin"
 set -gx GOPATH "$HOME/.go"
 fish_add_path "$GOPATH/bin"
-fish_add_path "/usr/local/go/bin"
+fish_add_path /usr/local/go/bin
 fish_add_path "$HOME/.cargo/bin"
 
 direnv hook fish | source
