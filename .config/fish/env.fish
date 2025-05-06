8aa69e2fe791c6d945ee76b871b6390a9a8a92b9
shino <sc@shinomin.me>
2025-05-06 20:12:50 +0700

    update fish env

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -6,5 +6,8 @@ set -gx LANG en_US.UTF-8
 fish_add_path "$HOME/.local/bin"
 set -gx GOPATH "$HOME/.go"
 fish_add_path "$GOPATH/bin"
-fish_add_path /usr/local/go/bin
 fish_add_path "$HOME/.cargo/bin"
+
+if command -q zoxide
+    zoxide init fish | source
+end
