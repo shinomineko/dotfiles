0a1637a990b065d9b759e804017703833d04062e
shino <sc@shinomin.me>
2026-03-25 18:46:50 +0700

    update fish env

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -6,9 +6,6 @@ set -gx LC_ALL en_US.UTF-8
 set -gx LANG en_US.UTF-8
 
 fish_add_path "$HOME/.local/bin"
-set -gx GOPATH "$HOME/.go"
-fish_add_path "$GOPATH/bin"
-fish_add_path "$HOME/.cargo/bin"
 
 if command -q zoxide
     zoxide init fish | source
